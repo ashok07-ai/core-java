694f128a9c5becb09e08e8c95cd3651c9e99c633
ashok07-ai <ak850860@gmail.com>
2025-01-07 22:52:29 +0545

    added the program for Hello world and Reading data from keyboards

---
+++ first-program/FirstProgram.java
@@ -0,0 +1,7 @@
+// This is a simple Java program that prints "Hello world" to the console.
+class FirstProgram {
+  public static void main(String args[]) {
+      // The main method is the entry point of the program.
+      System.out.println("Hello world"); // Prints "Hello world" to the console.
+  }
+}
